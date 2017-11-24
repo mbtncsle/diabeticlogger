31daacb163e6c77e6d8f2447dec8dbc9eaeb115f
tcamp1 <tcamp1@memphis.edu>
2017-11-24 00:44:16 -0600

    Database and table creation, population, and CRUD operations

---
+++ meal_item_crud.py
@@ -0,0 +1,151 @@
+import pyodbc
+import db
+
+table_name = "MealItem"
+
+
+class MealItemRecord:
+    """
+    Meal item record class with constructor
+    """
+    def __init__(self, meal_item_id=-1, meal_id=-1, description="", portions=0, carbs_per_portion=0, total_carbs=0):
+        self.meal_item_id = meal_item_id
+        self.meal_id = meal_id
+        self.description = description
+        self.portions = portions
+        self.carbs_per_portion = carbs_per_portion
+        self.total_carbs = total_carbs
+
+
+# ==============================================================================
+# Meal Item record operations
+# ==============================================================================
+def meal_item_insert(meal_item_record):
+    """
+    Inserts a meal item record
+    :param meal_item_record: MealItemRecord instance
+    :return: MealItemId of inserted record
+    """
+
+    sql_statement = "INSERT INTO " + table_name + \
+                    " (MealId, Description, Portions, CarbsPerPortion, TotalCarbs) OUTPUT INSERTED."
+    sql_statement = sql_statement + table_name + "Id"
+    sql_statement = sql_statement + " VALUES ("
+    sql_statement = sql_statement + str(meal_item_record.meal_id) + ", "
+    sql_statement = sql_statement + "'" + str(meal_item_record.description) + "', "
+    sql_statement = sql_statement + str(meal_item_record.portions) + ", "
+    sql_statement = sql_statement + str(meal_item_record.carbs_per_portion) + ", "
+    sql_statement = sql_statement + str(meal_item_record.total_carbs)
+    sql_statement = sql_statement + ");"
+
+    with db.Db() as cursor:
+        try:
+            cursor.execute(sql_statement)
+        except pyodbc.Error as ex:
+            print(ex.args)
+            return
+
+        return_id = cursor.fetchone()[0]
+        return return_id
+
+
+def meal_items_select_by_meal_id(meal_id):
+    """
+    Returns a list of records based on the MealId
+    :param meal_id: MealId of meal to retrieve records for
+    :return: List of MealRecords
+    """
+    sql_statement = "SELECT " + table_name + "Id, MealId, Description, Portions, CarbsPerPortion, TotalCarbs " + \
+                    "FROM " + table_name + " WHERE MealId = " + \
+                    str(meal_id) + ";"
+
+    with db.Db() as cursor:
+        try:
+            cursor.execute(sql_statement)
+        except pyodbc.Error as ex:
+            print(ex.args)
+            return
+
+        return_meal_items_records = []
+        row = cursor.fetchone()
+
+        while row:
+            return_meal_items_records.append(
+                MealItemRecord(
+                    meal_item_id=row.MealItemId,
+                    meal_id=row.MealId,
+                    description=row.Description,
+                    portions=row.Portions,
+                    carbs_per_portion=row.CarbsPerPortion,
+                    total_carbs=row.TotalCarbs
+                )
+            )
+            row = cursor.fetchone()
+
+    return return_meal_items_records
+
+
+def meal_item_select_by_id(meal_item_id):
+
+    """
+    Returns a meal item record based on MealItemId
+    :param meal_item_id: MealItemId of record
+    :return: MealItemRecord instance
+    """
+
+    sql_statement = "SELECT " + table_name + "Id, MealId, Description, Portions, CarbsPerPortion, TotalCarbs " + \
+                    "FROM " + table_name + " WHERE " + table_name + "Id = " + \
+                    str(meal_item_id) + ";"
+
+    with db.Db() as cursor:
+        try:
+            cursor.execute(sql_statement)
+        except pyodbc.Error as ex:
+            print(ex.args)
+            return
+
+        row = cursor.fetchone()
+
+        if row:
+            return MealItemRecord(
+                meal_item_id=row.MealItemId,
+                meal_id=row.MealId,
+                description=row.Description,
+                portions=row.Portions,
+                carbs_per_portion=row.CarbsPerPortion,
+                total_carbs=row.TotalCarbs
+            )
+        else:
+            return None
+
+
+def meal_item_delete(meal_item_id):
+    """
+    Deletes a meal item record based on MealItemId
+    :param meal_item_id: MealItemId of record
+    :return: Nothing is returned
+    """
+
+    sql_statement = "DELETE FROM " + table_name + " WHERE " + table_name + "Id = " + str(meal_item_id) + ";"
+
+    with db.Db() as cursor:
+        try:
+            cursor.execute(sql_statement)
+        except pyodbc.Error as ex:
+            print(ex.args)
+
+
+def meal_item_delete_by_meal_id(meal_id):
+    """
+    Deletes all meal item records for the associated MealId
+    :param meal_id: MealId of record
+    :return: Nothing is returned
+    """
+
+    sql_statement = "DELETE FROM " + table_name + " WHERE MealId = " + str(meal_id) + ";"
+
+    with db.Db() as cursor:
+        try:
+            cursor.execute(sql_statement)
+        except pyodbc.Error as ex:
+            print(ex.args)
---
+++ sleep_crud.py
@@ -0,0 +1,129 @@
+import pyodbc
+import datetime
+import db
+
+table_name = "Sleep"
+
+
+class SleepRecord:
+    """
+    Sleep record class with constructor
+    """
+    def __init__(self, sleep_id=-1, reading=0, record_date=datetime.datetime.now(), notes=""):
+        self.sleep_id = sleep_id
+        self.reading = reading
+        self.record_date = record_date
+        self.notes = notes
+
+
+# ==============================================================================
+# Sleep record operations
+# ==============================================================================
+def sleep_insert(sleep_record):
+    """
+    Inserts a sleep record
+    :param sleep_record: SleepRecord instance
+    :return: SleepId of inserted record
+    """
+
+    sql_statement = "INSERT INTO " + table_name + " (Reading, RecordDate, Notes) OUTPUT INSERTED."
+    sql_statement = sql_statement + table_name + "Id"
+    sql_statement = sql_statement + " VALUES ("
+    sql_statement = sql_statement + str(sleep_record.reading) + ", "
+    sql_statement = sql_statement + "'" + sleep_record.record_date.strftime("%Y-%m-%d %H:%M:%S") + "', "
+    sql_statement = sql_statement + "'" + str(sleep_record.notes) + "'"
+    sql_statement = sql_statement + ");"
+
+    with db.Db() as cursor:
+        try:
+            cursor.execute(sql_statement)
+        except pyodbc.Error as ex:
+            print(ex.args)
+            return
+
+        return_id = cursor.fetchone()[0]
+        return return_id
+
+
+def sleep_select_by_id(sleep_id):
+    """
+    Returns a sleep record based on SleepId
+    :param sleep_id: SleepId of record
+    :return: SleepRecord instance
+    """
+
+    sql_statement = "SELECT " + table_name + "Id, Reading, RecordDate, Notes FROM " +\
+                    table_name + " WHERE " + table_name + "Id = " + str(sleep_id) + ";"
+
+    with db.Db() as cursor:
+        try:
+            cursor.execute(sql_statement)
+        except pyodbc.Error as ex:
+            print(ex.args)
+            return
+
+        row = cursor.fetchone()
+
+        if row:
+            return SleepRecord(
+                sleep_id=row.SleepId,
+                reading=row.Reading,
+                record_date=row.RecordDate,
+                notes=row.Notes
+            )
+        else:
+            return None
+
+
+def sleep_select_by_days(days):
+    """
+    Returns a list of records from the previous number of days specified
+    :param days: days in the past to include
+    :return: List of SleepRecords
+    """
+    oldest_date = datetime.datetime.today() - datetime.timedelta(days=days)
+
+    sql_statement = "SELECT " + table_name + "Id, Reading, RecordDate, Notes" + \
+                    " FROM " + table_name + " WHERE RecordDate > \'{0}\' ORDER BY RecordDate DESC ;".format(
+                        oldest_date.strftime("%Y-%m-%d %H:%M:%S"))
+
+    with db.Db() as cursor:
+        try:
+            cursor.execute(sql_statement)
+        except pyodbc.Error as ex:
+            print(ex.args)
+            return
+
+        return_sleep_records = []
+        row = cursor.fetchone()
+
+        while row:
+            return_sleep_records.append(
+                SleepRecord(
+                    sleep_id=row.SleepId,
+                    reading=row.Reading,
+                    record_date=row.RecordDate,
+                    notes=row.Notes
+                )
+            )
+            row = cursor.fetchone()
+
+    return return_sleep_records
+
+
+def sleep_delete(sleep_id):
+    """
+    Deletes a sleep record based on SleepId
+    :param sleep_id: SleepId of record
+    :return: Nothing is returned
+    """
+
+    sql_statement = "DELETE FROM " + table_name + " WHERE " + table_name + "Id = " + str(sleep_id) + ";"
+
+    with db.Db() as cursor:
+        try:
+            cursor.execute(sql_statement)
+        except pyodbc.Error as ex:
+            print(ex.args)
+
+
