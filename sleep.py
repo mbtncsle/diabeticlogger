1da89ede86bcc9e2b08c87f4b06ff3ea283ca58a
Michael Bowman <mgbowman@memphis.edu>
2017-12-04 15:23:59 -0600

    Added design

--- sleep.py
+++ sleep.py
@@ -1,6 +1,6 @@
 import sys
 from PyQt5.QtWidgets import QLabel, QLineEdit, QDateEdit, QPushButton, QMdiSubWindow, QTimeEdit
-from PyQt5.QtCore import QTime
+from PyQt5.QtCore import QTime, Qt
 from datetime import datetime
 from InputWindow import InputWindow
 
@@ -9,10 +9,11 @@ class Sleep(InputWindow):
 	def __init__(self, parent):
 		super(Sleep, self).__init__(parent)
 
+		self.setFocusPolicy(Qt.NoFocus)
+
 		# Create the labels, setting their text, and their locations
 		self.sleep_lbl = QLabel(self)
 		self.sleep_lbl.setText("Please enter the hours of sleep")
-		self.sleep_lbl.move(77.5, 27)
 		self.sleep_lbl.adjustSize()
 
 		self.error_lbl = QLabel(self)
@@ -21,59 +22,50 @@ class Sleep(InputWindow):
                 
 		self.date_lbl = QLabel(self)
 		self.date_lbl.setText("Please pick a date")
-		self.date_lbl.move(450, 18)
 		self.sleep_lbl.adjustSize()
 		
 		self.time_lbl = QLabel(self)
 		self.time_lbl.setText("Please pick a time")
-		self.time_lbl.move(450, 137)
 		self.time_lbl.adjustSize()
 
 		# Create the editing box
 		self.sleep_qle = QLineEdit(self)
-		self.sleep_qle.move(107.5, 40)
 
 		# Create the date box
 		self.date_qde = QDateEdit(self)
-		self.date_qde.move(450, 40)
 
                 #Creating the time box
 		self.time = QTimeEdit(self)
 		self.time.setDisplayFormat("hh:mm")
 		self.time.setTime(QTime())
-		self.time.move(450, 150)
 		#self.time.adjustSize()
 
 		# Create the submit button
 		self.submit_qpb = QPushButton(self)
-		self.submit_qpb.move(450, 280)
 		self.submit_qpb.setText("Submit")
-		self.submit_qpb.adjustSize()
 
 		# Create the numpad buttons
 		self.backspace_qpb = QPushButton(self)
 		self.num_buttons = dict()
 		self.backspace_qpb.setText("Backspace")
-		self.backspace_qpb.move(107.5, 280)
 		self.backspace_qpb.clicked.connect(self.delete)
+		
 		for i in range(0, 10):
 			self.num_buttons["numpad" + str(i)] = QPushButton(self)
 			self.num_buttons["numpad" + str(i)].setText(str(i))
-			self.num_buttons["numpad" + str(i)].adjustSize()
-			self.num_buttons['numpad' + str(i)].move(75*i, 0)
 			self.num_buttons['numpad' + str(i)].clicked.connect(self.numbs)
 
-		self.num_buttons['numpad' + str(0)].move(20, 40)
-		self.num_buttons['numpad' + str(1)].move(40, 120)
-		self.num_buttons['numpad' + str(2)].move(120, 120)
-		self.num_buttons['numpad' + str(3)].move(200, 120)
-		self.num_buttons['numpad' + str(4)].move(40, 160)
-		self.num_buttons['numpad' + str(5)].move(120, 160)
-		self.num_buttons['numpad' + str(6)].move(200, 160)
-		self.num_buttons['numpad' + str(7)].move(40, 200)
-		self.num_buttons['numpad' + str(8)].move(120, 200)
-		self.num_buttons['numpad' + str(9)].move(200, 200)
-		self.num_buttons['numpad' + str(0)].move(120, 240)
+		self.num_buttons['numpad' + str(1)].move(100, 90)
+		self.num_buttons['numpad' + str(2)].move(140, 90)
+		self.num_buttons['numpad' + str(3)].move(180, 90)
+		self.num_buttons['numpad' + str(4)].move(100, 130)
+		self.num_buttons['numpad' + str(5)].move(140, 130)
+		self.num_buttons['numpad' + str(6)].move(180, 130)
+		self.num_buttons['numpad' + str(7)].move(100, 170)
+		self.num_buttons['numpad' + str(8)].move(140, 170)
+		self.num_buttons['numpad' + str(9)].move(180, 170)
+		self.num_buttons['numpad' + str(0)].setGeometry(40, 40, 80, 40)
+		self.num_buttons['numpad' + str(0)].move(100, 210)
 
 		# what happens when they press enter with the textbox selected
 		self.sleep_qle.returnPressed.connect(self.submit)
@@ -85,6 +77,15 @@ class Sleep(InputWindow):
 
 		# What happens when the submit button is clicked
 		self.submit_qpb.clicked.connect(self.submit)
+
+		self.sleep_lbl.setGeometry(115, 30, 25, 25)
+		self.date_lbl.setGeometry(290, 195, 25, 25)
+		self.time_lbl.setGeometry(293, 115, 25, 25)
+		self.sleep_qle.setGeometry(100, 45, 120, 40)
+		self.date_qde.setGeometry(275, 210, 120, 40)
+		self.submit_qpb.setGeometry(0, 294.5, 504, 30)
+		self.backspace_qpb.setGeometry(180, 210, 40, 40)
+		self.time.setGeometry(275, 130, 120, 40)
 
 		self.show()
 
