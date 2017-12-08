17165da6f2b3b1128d4fba831df32a001676b226
Michael Bowman <mgbowman@memphis.edu>
2017-12-07 21:37:14 -0600

    Added changed inputwindow

--- InputWindow.py
+++ InputWindow.py
@@ -2,8 +2,6 @@ from PyQt5.QtWidgets import QMdiSubWindow, QPushButton, QDialog, QLabel, QLineEd
 from PyQt5.QtCore import Qt, pyqtSlot, QTime
 from datetime import datetime
 import sys
-sys.path.insert(0, "./database_files")
-import blood_glucose_crud, meal_crud, meal_item_crud, sleep_crud, steps_crud
 
 class InputWindow(QMdiSubWindow):
 	
@@ -38,6 +36,8 @@ class InputWindow(QMdiSubWindow):
 		self.time_lbl = QLabel(self)
 		self.time_lbl.setText("Please pick a time")
 		self.time_lbl.adjustSize()
+
+		self.unit_lbl = QLabel(self)
 
 		# Create the editing box
 		self.main_qle = QLineEdit(self)
@@ -95,12 +95,17 @@ class InputWindow(QMdiSubWindow):
 		self.main_lbl.move(100, 30)
 		self.date_lbl.move(290, 195)
 		self.time_lbl.move(293, 115)
+		self.unit_lbl.move(225, 45)
 		self.main_qle.setGeometry(100, 45, 120, 40)
 		self.date_qde.setGeometry(275, 210, 120, 40)
 		self.submit_qpb.setGeometry(0, 294.5, 504, 30)
 		self.backspace_qpb.setGeometry(180, 210, 40, 40)
 		self.time.setGeometry(275, 130, 120, 40)
 		self.error_lbl.move(255, 85)
+
+	def set_error(self, text):
+		self.error_lbl.setText(text)
+		self.error_lbl.adjustSize()
 
 	@pyqtSlot()
 	def numbs(self):
@@ -112,28 +117,16 @@ class InputWindow(QMdiSubWindow):
 		if len(string) > 0:
 			self.main_qle.setText(string[:-1])
 
-	# Log the given input
-	def log_input(self, input_type, data):
-		dt = datetime.strptime(self.date_qde.date().toString("yyyy-MM-dd") + " " + self.time.time().toString("hh:mm") + ":00", "%Y-%m-%d %H:%M:%S")
-		breakfast = 8
-		lunch = 12
-		dinner = 20
-		if input_type == self.blood_glucose:
-			blood_glucose_crud.blood_glucose_insert(blood_glucose_crud.BloodGlucoseRecord(meal = data[0], reading = int(self.main_qle.text()), record_date = dt))
-		elif input_type == self.sleep:
-			sleep_crud.sleep_insert(sleep_crud.SleepRecord(reading = int(self.main_qle.text()), record_date = dt))
-		elif input_type == self.exercise:
-			steps_crud.steps_insert(steps_crud.StepsRecord(reading = int(self.main_qle.text()), record_date = dt))
-		else:
-			id = None
-			for m in meal_crud.meal_select_by_days((datetime.now() - dt).days + 1):
-				if m.record_date == dt:
-					id = m.meal_id
-					break
-			if id == None:
-				id = meal_crud.meal_insert(meal_crud.MealRecord(meal = data[2], reading = int(self.main_qle.text()) * int(data[0]), record_date = dt))
-			meal_item_crud.meal_item_insert(meal_item_crud.MealItemRecord(meal_id = id, description = data[1], portions = int(self.main_qle.text()), carbs_per_portion = int(data[0]), total_carbs = int(self.main_qle.text()) * int(data[0])))
+	# Return all inputs necessary for logging inputs
+	def get_inputs(self):
+		return [self.main_qle.text(), self.date_qde.date(), self.time.time()]
+
+	# Call parent update
+	def update(self):
 		self.parent.update_data()
+
+	# Create a notification window for submission
+	def submit_notify(self):
 		note = QDialog()
 		note_lbl = QLabel(note)
 		note_button = QPushButton("Okay", note)
@@ -142,3 +135,12 @@ class InputWindow(QMdiSubWindow):
 		note_button.move(0, 25)
 		note_button.clicked.connect(note.accept)
 		note.exec_()
+
+	# Log the given input
+	def log_input(self, input_type, data):
+		self.set_error("")
+		self.update()
+
+	def set_units(self, unit):
+		self.unit_lbl.setText(unit)
+		self.adjustSize()
